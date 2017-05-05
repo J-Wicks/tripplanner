41fba11deb5b0b0312a93337a854d7943477f5ee
Jeremy Wicks <artfullu@gmail.com>
2017-05-05 12:00:01 -0400

    Initial Upload

---
+++ app.js
@@ -0,0 +1,29 @@
+var express = require ('express');
+var app = express();
+var bodyparser = require ('body-parser');
+// var router = express.Router();
+var morgan = require('morgan');
+const PORT = 8080;
+
+
+app.listen(PORT);
+console.log('Listening on ' + PORT);
+
+app.use(morgan('short'));
+
+app.use(bodyparser.json())
+app.use(bodyparser.urlencoded({default: true}));
+
+
+app.get('/', function(req, res, next){
+	res.send('Welcome to the Homepage!');
+}, function(next){});
+
+app.use(function (err, req, res, next){
+	res.status(err.status || 500);
+	console.error(err);
+	res.send (err.status);
+});
+
+app.use(express.static(__dirname + '/public'));
+
---
+++ models/index.js
@@ -0,0 +1,2 @@
+const Sequelize = require ('sequelize');
+var db = sequelize('http://localhost:5432/');
